16f996320cf6fa30de680965ea21b22add0337e6
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2016-09-09 16:25:53 +0900

    Update spec

--- spec/ensure_xcode_build_version_action_spec.rb
+++ spec/ensure_xcode_build_version_action_spec.rb
@@ -1,9 +1,11 @@
 describe Fastlane::Actions::EnsureXcodeBuildVersionAction do
   describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The ensure_xcode_build_version plugin is working!")
+    it 'ensures xcode build version' do
+      expect(Fastlane::UI).to receive(:message).with(/Selected Xcode Build version is not correct/)
+      expect(Fastlane::UI).to receive(:message).with("To correct this, use: `xcode_select(version: 8A218a)`.")
+      expect(Fastlane::UI).to receive(:user_error!).with(/Selected Xcode Build version doesn't match your requirement./)
 
-      Fastlane::Actions::EnsureXcodeBuildVersionAction.run(nil)
+      Fastlane::Actions::EnsureXcodeBuildVersionAction.run(build_version: '8A218a')
     end
   end
 end
