a3a737c32f14c817b47c858420e7a120b4777f0a
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2016-09-09 15:58:23 +0900

    Update action

--- lib/fastlane/plugin/ensure_xcode_build_version/actions/ensure_xcode_build_version_action.rb
+++ lib/fastlane/plugin/ensure_xcode_build_version/actions/ensure_xcode_build_version_action.rb
@@ -2,11 +2,29 @@ module Fastlane
   module Actions
     class EnsureXcodeBuildVersionAction < Action
       def self.run(params)
-        UI.message("The ensure_xcode_build_version plugin is working!")
+        required_build_version = params[:build_version]
+
+        selected_build_version = sh "xcversion selected | tail -1 | xargs echo -n"
+
+        versions_match = selected_build_version == "Build version #{required_build_version}"
+
+        if versions_match
+          UI.success("Selected Xcode Build version is correct: #{selected_build_version}")
+        else
+          UI.message("Selected Xcode Build version is not correct: #{selected_build_version}. You expected #{required_build_version}.")
+          UI.message("To correct this, use: `xcode_select(version: #{required_build_version})`.")
+
+          UI.user_error!("Selected Xcode Build version doesn't match your requirement.\nExpected: Build version #{required_build_version}\nActual: #{selected_build_version}\n")
+        end
       end
 
       def self.description
-        "Ensure Xcode Build Version for working with Beta, GM and Release"
+        "Ensure the selected Xcode Build version with xcode-select matches a value"
+      end
+
+      def self.details
+        "If building your app requires a specific version of Xcode, you can invoke this command before using gym.\n
+        For example, to ensure that a beta version is not accidentally selected to build, which would make uploading to TestFlight fail."
       end
 
       def self.authors
@@ -15,20 +33,25 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "ENSURE_XCODE_BUILD_VERSION_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+          FastlaneCore::ConfigItem.new(key: :build_version,
+                                       env_name: "FL_ENSURE_XCODE_BUILD_VERSION",
+                                       description: "Xcode Build version to verify that is selected",
+                                       is_string: true,
+                                       optional: false)
         ]
       end
 
+      def self.output
+        [
+          ['FL_ENSURE_XCODE_BUILD_VERSION', 'Xcode Build version to verify that is selected']
+        ]
+      end
+
+      def self.return_value
+      end
+
       def self.is_supported?(platform)
-        # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
-        # See: https://github.com/fastlane/fastlane/blob/master/fastlane/docs/Platforms.md
-        #
-        # [:ios, :mac, :android].include?(platform)
-        true
+        [:ios, :mac].include?(platform)
       end
     end
   end
