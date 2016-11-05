d1c0fb2531dc1565b5daeaf815644b1122d9827e
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2016-11-05 14:39:47 +0900

    Add xcversion-safe.sh

---
+++ lib/assets/xcversion-safe.sh
@@ -0,0 +1,15 @@
+# Since Xcode has a dependency to 2 external gems: sqlite and CFPropertyList
+# More information https://github.com/fastlane/fastlane/issues/6495
+# We have to unset those variables for rbenv, rvm and when the user uses bundler
+unset RUBYLIB
+unset RUBYOPT
+unset BUNDLE_BIN_PATH
+unset _ORIGINAL_GEM_PATH
+unset BUNDLE_GEMFILE
+# Even if we do not use rbenv in some environments such as CircleCI,
+# We also need to unset GEM_HOME and GEM_PATH explicitly.
+# More information https://github.com/fastlane/fastlane/issues/6277
+unset GEM_HOME
+unset GEM_PATH
+
+xcversion selected | tail -1 | xargs echo -n
