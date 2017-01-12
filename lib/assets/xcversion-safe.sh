8457236f57c1466132e77d4790c9feab3c33745e
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2017-01-12 19:48:00 +0900

    Update xcversion-safe.sh to run xcversion correctly

--- lib/assets/xcversion-safe.sh
+++ lib/assets/xcversion-safe.sh
@@ -3,13 +3,13 @@
 # We have to unset those variables for rbenv, rvm and when the user uses bundler
 # If we unset ruby related path, we got errors:
 # /Users/distiller/ookami-ios/vendor/bundle/ruby/2.3.0/bin/xcversion:22:in `<main>': undefined method `activate_bin_path' for Gem:Module (NoMethodError)
-# unset RUBYLIB
-# unset RUBYOPT
+unset RUBYLIB
+unset RUBYOPT
 # If we unset Bundle related path, we got errors:
 # /Users/distiller/ookami-ios/vendor/bundle/ruby/2.3.0/bin/xcversion:22:in `<main>': undefined method `activate_bin_path' for Gem:Module (NoMethodError)
-# unset BUNDLE_BIN_PATH
-# unset _ORIGINAL_GEM_PATH
-# unset BUNDLE_GEMFILE
+unset BUNDLE_BIN_PATH
+unset _ORIGINAL_GEM_PATH
+unset BUNDLE_GEMFILE
 # Even if we do not use rbenv in some environments such as CircleCI,
 # We also need to unset GEM_HOME and GEM_PATH explicitly.
 # More information https://github.com/fastlane/fastlane/issues/6277
