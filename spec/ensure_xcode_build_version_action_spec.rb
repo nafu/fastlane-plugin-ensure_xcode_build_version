d4b9628bc7078ff16f0e4a8b043bfc1f9ad77f71
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2017-07-29 02:20:19 +0900

    Revise rspecs

--- spec/ensure_xcode_build_version_action_spec.rb
+++ spec/ensure_xcode_build_version_action_spec.rb
@@ -5,7 +5,7 @@ describe Fastlane::Actions::EnsureXcodeBuildVersionAction do
     let(:different_response) { "Xcode 7.3\nBuild version 34a893" }
 
     it "is successful when the build version matches" do
-      expect(Fastlane::Actions::EnsureXcodeBuildVersionAction).to receive(:sh).and_return(matching_response)
+      allow(Fastlane::Actions::EnsureXcodeBuildVersionAction).to receive(:sh).and_return(matching_response)
       expect(Fastlane::UI).to receive(:success).with(/Driving the lane/)
       expect(Fastlane::UI).to receive(:success).with(/Selected Xcode Build version is correct/)
 
@@ -15,7 +15,7 @@ describe Fastlane::Actions::EnsureXcodeBuildVersionAction do
     end
 
     it "matches even when there is extra output" do
-      expect(Fastlane::Actions::EnsureXcodeBuildVersionAction).to receive(:sh).and_return(matching_response_extra_output)
+      allow(Fastlane::Actions::EnsureXcodeBuildVersionAction).to receive(:sh).and_return(matching_response_extra_output)
       expect(Fastlane::UI).to receive(:success).with(/Driving the lane/)
       expect(Fastlane::UI).to receive(:success).with(/Selected Xcode Build version is correct/)
 
@@ -25,12 +25,37 @@ describe Fastlane::Actions::EnsureXcodeBuildVersionAction do
     end
 
     it "presents an error when the build version does not match" do
-      expect(Fastlane::Actions::EnsureXcodeBuildVersionAction).to receive(:sh).and_return(different_response)
+      allow(Fastlane::Actions::EnsureXcodeBuildVersionAction).to receive(:sh).and_return(different_response)
       expect(Fastlane::UI).to receive(:user_error!)
 
       Fastlane::FastFile.new.parse("lane :test do
         ensure_xcode_build_version(build_version: '8A218a')
       end").runner.execute(:test)
+    end
+
+    context "require input When no build_version passed" do
+      it "is successful when the input matches" do
+        allow(Fastlane::Actions::EnsureXcodeBuildVersionAction).to receive(:sh).and_return(matching_response)
+        allow(Fastlane::UI).to receive(:input).and_return("8A218a")
+        expect(Fastlane::UI).to receive(:user_error!).with(/No value found for 'build_version'/)
+        expect(Fastlane::UI).to receive(:success).with(/Driving the lane/)
+        expect(Fastlane::UI).to receive(:success).with(/Selected Xcode Build version is correct/)
+
+        Fastlane::FastFile.new.parse("lane :test do
+          ensure_xcode_build_version
+        end").runner.execute(:test)
+      end
+
+      it "presents an error when the input version does not match" do
+        allow(Fastlane::Actions::EnsureXcodeBuildVersionAction).to receive(:sh).and_return(matching_response)
+        allow(Fastlane::UI).to receive(:input).and_return("\n")
+        expect(Fastlane::UI).to receive(:user_error!).with(/No value found for 'build_version'/)
+        expect(Fastlane::UI).to receive(:user_error!).with(/Selected Xcode Build version doesn't match your requirement/)
+
+        Fastlane::FastFile.new.parse("lane :test do
+          ensure_xcode_build_version
+        end").runner.execute(:test)
+      end
     end
   end
 end
